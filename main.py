91e1a33efb651bd77c024a7b3aea3ffa245e102c
ck@acedev <chandankumar75707@gmail.com>
2023-09-07 13:36:56 +0530

    cors

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73c7ed60a8020dbe2c7dd609e069f1bde85806d9f951c96a0c2e8bfee0acb6ae
-size 528
+oid sha256:8eb69216bb117b4d7f0631e9a10377de5c58a40143e3af92f54cc6d8d217fea9
+size 535
--- tomato.py
+++ tomato.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a20f0f38836fade00f7af9f6d6847fdab9d18aeb70e4ac002a905d32ba953f09
-size 737
+oid sha256:e86adbcfd05f2366170841eca20291eac925870f1a2650c7af3429614e9a38ae
+size 719
