3dbaf1cb74de0bcc5adea861ca6ba18101ed55a8
ck@acedev <chandankumar75707@gmail.com>
2023-10-09 16:13:44 +0530

    modification in image size

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaa9152d5c5fc34069ac95848361c3fbfa88881bca473cca69f45c65a3db5fea
-size 616
+oid sha256:1eaead0665e307f917f686fc2ae9afd61d601b1fec7fe6e6e8a3f85fd762af4b
+size 708
